fa4a752893382fa95a1552f815a4be3da419ea8d
Alexandr Ivanov <aivanov@informatica.ru>
2016-06-02 20:36:45 +0300

    init

---
+++ server-dev.js
@@ -0,0 +1,41 @@
+import WebpackDevServer from 'webpack-dev-server';
+import webpack from 'webpack';
+import webpackConfig from './webpack.config';
+import http from 'http';
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import router from './lib/router';
+// import auth from './lib/auth';
+
+const app = express();
+app.use(cors({origin: true}));
+// auth(app);
+
+app.use(bodyParser.json({ limit: '1024mb' }));
+
+app.use(router);
+
+const httpServer = http.Server(app);
+
+webpackConfig.output.path = '/';
+
+const compiler = webpack(webpackConfig);
+
+const server = new WebpackDevServer(compiler, {
+  contentBase: './dist',
+  hot: true,
+  historyApiFallback: true,
+  proxy: {
+    '/api/*': 'http://localhost:4000'
+  }
+});
+
+server.listen(8080, () => {
+  httpServer.listen(4000);
+/* eslint-disable no-console */
+console.log('App server listening on port 8080');
+console.log('Build app...');
+
+/* eslint-enable no-console */
+});
