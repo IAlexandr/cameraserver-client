630d78a1f061080179ddf4c46051e35cace79afd
Tetarenko Eugene <eugene.tetarenko@yandex.ru>
2017-01-25 15:52:03 +0300

    another manifest generation

--- router.js
+++ router.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const moment = require('moment');
 const options = require('./options');
 const getConnections = require('./lib/server/getConnections');
 const generateManifest = require('./lib/server/generateManifest');
+const getArchiveMpd = require('./lib/server/get-archive-mpd');
 const connect = require('./lib/server/connect');
-
 var connections;
 
 const router = new express.Router();
@@ -53,6 +54,83 @@ router.get('/streams/:cameraId/manifest.mpd', (req, res) => {
     .catch(function (err) {
       res.sendStatus(500);
     });
+});
+
+router.get('/archive/:cameraId/manifest.mpd', (req, res) => {
+  const cameraId = req.params.cameraId;
+  const query = req.query;
+
+  const startDate = new Date(parseInt(query.dt));
+  const endDate = moment(startDate);
+  endDate.set('hour', 23);
+  endDate.set('minute', 59);
+  endDate.set('second', 59);
+  endDate.set('millisecond', 0);
+
+  connections.db.Session.findAll({
+    where: {
+      $and: {
+        createdAt: {
+          $gte: startDate
+        },
+        createdAt: {
+          $lte: endDate.utc().format()
+        },
+        cameraId: cameraId
+      }
+    }
+  })
+    .then((records) => {
+      const guids = records.map((item) => {
+        return item.sessionGuid;
+      });
+      connections.db.Segment.findAll({
+        order: '"chunkNumber" ASC',
+        where: {
+          sessionGuid: {
+            $in: guids
+          },
+          $and: {
+            createdAt: {
+              $gte: startDate
+            },
+            createdAt: {
+              $lte: endDate.utc().format()
+            }
+          }
+        }
+      }).then((segments) => {
+        if (records.length) {
+          res.set('Content-Type', 'text/xml');
+          return res.send(getArchiveMpd(records, segments));
+        } else {
+          return res.sendStatus(404);
+        }
+      }).catch(function (err) {
+        res.sendStatus(500);
+      });
+    })
+    .catch(function (err) {
+      res.sendStatus(500);
+    });
+});
+
+router.get('/archive/:cameraId/:key', (req, res) => {
+  const key = req.params.key;
+
+  connections.store.exists({ key }, (err, exist) => {
+    if (err) {
+      return res.status(500);
+    }
+
+    if (!exist) {
+      return res.status(404);
+    }
+
+    const rs = connections.store.createReadStream({ key });
+
+    rs.pipe(res);
+  });
 });
 
 router.get('/streams/:cameraId/:key', (req, res) => {
